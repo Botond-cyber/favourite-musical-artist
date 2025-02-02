7ef25a65357fdd0bb2744bb25cc607ce5934b572
botond <david.botond.bajan@students.jedlik.eu>
2025-02-02 10:45:20 +0100

    fixed the fucking siderbar in about.html

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const barBtn = document.querySelector('.barcontainer');
 const nav = document.querySelector('.aboutNav');
 
+window.onload = function() {
+    nav.style.display = 'none';
+};
+
+
 barBtn.onclick = function() {
     if (nav.style.display === 'block') {
         nav.style.display = 'none';
