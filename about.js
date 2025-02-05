0467f1b025a01bef8b754d4e20907a49f7e4a96f
botond <david.botond.bajan@students.jedlik.eu>
2025-02-05 21:26:30 +0100

    made some changes to aboutnav

--- about.js
+++ about.js
@@ -1,6 +1,11 @@
 const barBtn = document.querySelector('.barcontainer');
 const nav = document.querySelector('.aboutNav');
+const barcheck = document.getElementById('barcheck');
 
+window.onload = function() {
+    nav.style.display = 'none';
+    barcheck.checked = false;
+};
 
 barBtn.onclick = function() {
     if (nav.style.display === 'block') {
